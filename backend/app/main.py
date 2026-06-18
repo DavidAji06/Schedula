f2452b6a94cad52cd50639980afb2b2538aa4f5d
DavidAji06 <davidaji309@gmail.com>
2026-06-18 22:31:41 +0100

    CORS fix for vercel frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,27 +1,23 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.database import Base, engine
-from app.routers import auth, events, tasks
 from app.routers import auth, events, tasks, ai
 
 app = FastAPI(title="Schedula API")
 
-# CORS -- allows your React frontend to talk to the API
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-    "http://localhost:5173",
-    "https://schedula-6qht.onrender.com",
-],
+        "http://localhost:5173",
+        "https://schedula-xi.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Create all tables on startup
 Base.metadata.create_all(bind=engine)
 
-# Include routers
 app.include_router(auth.router)
 app.include_router(events.router)
 app.include_router(tasks.router)
